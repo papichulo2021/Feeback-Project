56a77cf3e493d8fbe1926de8dda2ad166798ff71
Edgar Martinez <96887405+papichulo2021@users.noreply.github.com>
2025-01-23 20:53:17 -0800

    Real Time Validation

--- Feedback/src/components/FeedbackForm.jsx
+++ Feedback/src/components/FeedbackForm.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import Card from './shared/Card'
+import { useState } from 'react';
+import Card from './shared/Card';
 import Button from './shared/Button';
 
 function FeedbackForm() {
     const [text, setText] = useState('');
-
+    const [btnDisabled, setBtnDisabled] = useState(true);
+    const [message, setMessage] = useState('');
+    
     const handleTextChange = (e) => {
-        setText(e.target.value)
-    }
+        const inputText = e.target.value;
+
+        if (inputText === '') {
+            setBtnDisabled(true);
+            setMessage(''); // Clear the message when input is empty
+        } else if (inputText.trim().length <= 10) {
+            setMessage('Text must be at least 10 characters');
+            setBtnDisabled(true);
+        } else {
+            setMessage(''); // Clear the message when input is valid
+            setBtnDisabled(false);
+        }
+
+        setText(inputText);
+    };
 
     return (
-    <Card>
-        <form>
-            <h2>How would you rate your services with us?</h2>
-            {/* rating select component*/}
-            <div className="input-group">
-                <input 
-                    onChange={handleTextChange} 
-                    type="text"
-                    placeholder='Write a review'
-                    value={text}
-                />
-                <Button 
-                    type='submit'
-                    
-                >
-                    Send</Button>
-            </div>
-        </form>
-    </Card>
-  )
+        <Card>
+            <form>
+                <h2>How would you rate your services with us?</h2>
+                {/* rating select component */}
+                <div className="input-group">
+                    <input 
+                        onChange={handleTextChange} 
+                        type="text"
+                        placeholder="Write a review"
+                        value={text}
+                    />
+                    <Button 
+                        type="submit"
+                        isDisabled={btnDisabled}
+                    >
+                        Send
+                    </Button>
+                </div>
+                {message && <div className="message">{message}</div>}
+            </form>
+        </Card>
+    );
 }
 
-export default FeedbackForm
+export default FeedbackForm;
